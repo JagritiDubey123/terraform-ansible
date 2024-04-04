de627a714cbb05a5cfd06d038a503000731c2359
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-04-04 17:46:51 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -5,8 +5,6 @@ resource "google_compute_instance" "vm_instance" {
     name = "instance-${count.index + 1}"
   machine_type = var.machine_type
   zone         = var.zone
-  # boot_disk = var.boot_disk
-  # Add other configuration attributes as needed
   boot_disk {
     initialize_params {
       image = var.boot_disk
@@ -21,42 +19,4 @@ resource "google_compute_instance" "vm_instance" {
 output "vm_count" {
   value = var.vm_count
 }
-
-# output "instance_names" {
-#   description = "Names of the created VM instances"
-#   value       = [google_compute_instance.vm_instance[*].name]
-# }
-# output "vm_count" {
-#   description = "Number of VM instances to create"
-#   value       = var.vm_count
-# }
-
-# output "machine_type" {
-#   description = "Machine type for the VM instances"
-#   value       = var.machine_type
-# }
-
-# output "boot_disk" {
-#   description = "Boot disk for the VM instances"
-#   value       = var.boot_disk
-# }
-
-# output "zone" {
-#   description = "Zone for the VM instances"
-#   value       = var.zone
-# }
-
-# output "network_name" {
-#   description = "Network name for the VM instances"
-#   value       = var.network_name
-# }
-
-# output "subnet_name" {
-#   description = "Subnet name for the VM instances"
-#   value       = var.subnet_name
-# }
-
-
-
-# # modules/vm/main.tf
 
