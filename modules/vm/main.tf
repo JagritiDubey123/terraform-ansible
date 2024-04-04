cd236bc94b5400ce17f0b1f77dcba31d7d1c2901
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-04-04 17:45:56 +0530

    Add files via upload

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,1 +1,62 @@
-jbhkhdflknhfkj
+# modules/vm/main.tf
+
+resource "google_compute_instance" "vm_instance" {
+  count         = var.vm_count
+    name = "instance-${count.index + 1}"
+  machine_type = var.machine_type
+  zone         = var.zone
+  # boot_disk = var.boot_disk
+  # Add other configuration attributes as needed
+  boot_disk {
+    initialize_params {
+      image = var.boot_disk
+    }
+    }
+    network_interface {
+    network = var.network_name
+    subnetwork = var.subnet_name
+    access_config {}
+  }
+}
+output "vm_count" {
+  value = var.vm_count
+}
+
+# output "instance_names" {
+#   description = "Names of the created VM instances"
+#   value       = [google_compute_instance.vm_instance[*].name]
+# }
+# output "vm_count" {
+#   description = "Number of VM instances to create"
+#   value       = var.vm_count
+# }
+
+# output "machine_type" {
+#   description = "Machine type for the VM instances"
+#   value       = var.machine_type
+# }
+
+# output "boot_disk" {
+#   description = "Boot disk for the VM instances"
+#   value       = var.boot_disk
+# }
+
+# output "zone" {
+#   description = "Zone for the VM instances"
+#   value       = var.zone
+# }
+
+# output "network_name" {
+#   description = "Network name for the VM instances"
+#   value       = var.network_name
+# }
+
+# output "subnet_name" {
+#   description = "Subnet name for the VM instances"
+#   value       = var.subnet_name
+# }
+
+
+
+# # modules/vm/main.tf
+
---
+++ modules/vm/variables.tf
@@ -0,0 +1,37 @@
+# modules/vm/variables.tf
+
+# variable "name" {
+#   description = "The name of the VM instance."
+#   type        = string
+# }
+variable "vm_count" {
+  description = "The number of VM instances to create."
+  type        = number
+}
+
+variable "machine_type" {
+  description = "The machine type of the VM instance."
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone of the VM instance."
+  type        = string
+}
+variable "region" {
+  description = "The zone of the VM instance."
+  type        = string
+}
+variable "boot_disk" {
+    type = string
+    description = "The region of the VM instance"
+}
+variable "network_name" {
+  type = string
+  description = "The name of the network to which the VM instance will be attached."
+}
+
+variable "subnet_name" {
+   type = string
+  description = "The name of the subnet to which the VM instance will be attached."
+}
